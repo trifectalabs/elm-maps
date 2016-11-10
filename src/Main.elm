a0f3757aea8d86f7ee952406e059ea167b3aabd3
Christopher Poenaru <kiambogo@gmail.com>
2016-11-10 15:10:22 -0500

    GeoJson parsing and rendering
      - Helper methods for parsing GeoJson
      - Basic rendering of single polygon with elm-svg
      - Unit tests for GeoJson parsing

---
+++ src/Main.elm
@@ -0,0 +1,82 @@
+module Main exposing (..)
+
+import String exposing (append)
+import Maybe exposing (andThen)
+import Json.Encode
+import Task exposing (perform)
+import Http
+import Html.App as App
+import Svg exposing (polygon)
+import Html exposing (Html)
+import Svg.Attributes exposing (version, x, y, viewBox, fill, points)
+import GeoJson exposing
+  ( GeoJson, GeoJsonObject(..), Geometry(..), FeatureObject
+  , decoder
+  )
+import GeoJsonHelpers exposing (..)
+import Debug exposing (log)
+
+
+sampleUrl : String
+sampleUrl =
+    "http://localhost:3000/sample.json"
+
+
+main : Program Never
+main =
+    App.program
+        { init = init , view = view , update = update , subscriptions = subscriptions }
+
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+-- Model
+
+type alias Model = GeoJson
+
+init : ( Model, Cmd Msg )
+init =
+    ( ( (Geometry(Point(0,0,[]) ) ), Nothing ) , fetchPerform sampleUrl )
+
+
+-- Update
+
+type Msg
+    = FetchSuccess GeoJson
+    | FetchFail Http.Error
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    FetchSuccess geoJson ->
+      (geoJson, Cmd.none)
+
+    FetchFail err ->
+      let
+        error = log "err" err
+      in
+        log "fail" (model, Cmd.none)
+
+
+-- View
+
+view : Model -> Html Msg
+view model =
+  let
+    svgPolys = generatePolygonStrings model
+      |> List.map (\p -> polygon [ fill "#F0AD00", points p ] [])
+      |> Svg.svg []
+  in
+    Svg.svg
+      [ version "1.1" , x "0" , y "0" , viewBox "0 0 180 180" ] [ svgPolys ]
+
+
+fetchPerform : String -> Cmd Msg
+fetchPerform url =
+  Task.perform FetchFail FetchSuccess (Http.get decoder url)
+
